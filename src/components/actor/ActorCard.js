6558ec1de7a69b8de2b70878c3d0316132db337b
tk <trishank01@gmail.com>
2022-08-22 13:07:55 +0530

    installed styled component and design actor and show page

--- src/components/actor/ActorCard.js
+++ src/components/actor/ActorCard.js
@@ -1,18 +1,19 @@
 import React from 'react'
+import { StyledActorCard}   from './ActorCard.styled'
 
 const ActorCard = ({image, name,gender,country,birthday,deathday}) => {
   return (
-    <div>
-        <div>
+    <StyledActorCard>
+        <div className='img-wrapper'>
         <img src={image} alt="Actor" />
       </div>
         <h1>
           {name} {gender ? `(${gender})` : null}
         </h1>
         <p>{country ? `Comes from ${country}` : "No Country Known"}</p>
-        {birthday ? <p>Born{birthday}</p> : null}
-        <p>{deathday ? `Died ${deathday}` : 'Alive'}</p>
-    </div>
+        {birthday ? <p>Born {birthday}</p> : null}
+        <p className='deathday'>{deathday ? `Died ${deathday}` : 'Alive'}</p>
+    </StyledActorCard>
   )
 }
 
