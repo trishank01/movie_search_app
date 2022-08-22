4e9283d26ff0516f1631ce7f8ea61a7b758ff534
tk <trishank01@gmail.com>
2022-08-23 01:57:41 +0530

    added show pages with cast details and more

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,5 +1,9 @@
 import React, { useEffect, useState, useReducer } from "react";
 import { useParams } from "react-router-dom";
+import Cast from "../components/show/Cast";
+import Details from "../components/show/Details";
+import Seasons from "../components/show/Seasons";
+import ShowMainData from "../components/show/ShowMainData";
 import { apiGet } from "../misc/config";
 
 const reducer = (prevState, action) => {
@@ -56,14 +60,37 @@ const Show = () => {
     };
   }, [id]);
 
-
   if (isLoading) {
     return <div>Data is being Loaded</div>;
   }
   if (error) {
     return <div>error occured : {error}</div>;
   }
-  return <div>this is show page</div>;
+  return (
+    <div>
+      <ShowMainData
+        image={show.image}
+        name={show.name}
+        rating={show.rating}
+        summer={show.summary}
+        tags={show.genres}
+      />
+      <div>
+        <h2>Details</h2>
+        <Details status={show.status} network={show.network} />
+      </div>
+
+      <div>
+        <h2>Seasons</h2>
+        <Seasons seasons={show._embedded.seasons} />
+      </div>
+
+      <div>
+        <h2>Cast</h2>
+        <Cast cast={show._embedded.cast} />
+      </div>
+    </div>
+  );
 };
 
 export default Show;
